333ab1fcc2a0f200dd476940898b2eee508055b6
Paulo <paulojjj@gmail.com>
2020-05-12 19:17:14 -0300

    version 0.2.0.0

--- src/main/java/paulojjj/solarenergy/Main.java
+++ src/main/java/paulojjj/solarenergy/Main.java
@@ -22,7 +22,7 @@ import paulojjj.solarenergy.registry.Items;
 public class Main {
 	public static final String MODID = "solarenergy";
 	public static final String NAME = "Solar Energy";
-	public static final String VERSION = "0.1.0.2";
+	public static final String VERSION = "0.2.0.0";
 
 	public static SoundEvent sound = null;
 	
